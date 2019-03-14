c9889465a49b8fffdf7eb580ab7553076a240f64
Nerijus Gabalis <ner147896523@gmail.com>
2019-03-14 13:25:15 +0200

    SEO meta tags.

---
+++ Sourkick-Coming-Soon-Landing/index.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <?php
+        $metatitle = "Sourkick";
+        $metadescription = "";
+        $metakeywords = "";
+        $metaimage = "https:///assets/images/bg.jpg";
+        $metalink = "https;//";
+    ?>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no">
+    <meta name="description" content="<?=$metadescription;?>">
+    <meta name="keywords" content="<?=$metakeywords;?>">
+    <meta name="author" content="<?=$metatitle;?>">
+    <meta name="DC.title" lang="LT" content='<?=$metatitle;?>'>
+    <meta name="DC.description" CONTENT="<?=$metadescription;?>">
+    <meta name="DC.keywords" CONTENT="<?=$metakeywords;?>">
+    <meta property="og:image" content="<?=$metaimage;?>"/>
+    <meta property="og:image:type" content="image/jpg" />
+    <meta property="og:description" content="<?=$metadescription;?>" />
+    <meta property="og:title" content="<?=$metatitle;?>" />
+    <meta property="og:type" content="website" />
+    <meta property="og:url" content="<?=$metalink;?>">
+    <meta property="og:site_name" content="<?=$metatitle;?>">
+    <meta name="twitter:card" content="summary">
+    <meta name="twitter:image" content="<?=$metaimage;?>" />
+    <meta name="twitter:title" content="<?=$metatitle;?>" />
+    <meta name="twitter:description" content="<?=$metadescription;?>" />
+    <meta name="twitter:site" content="<?=$metalink;?>">
+    <link rel="canonical" href="<?=$metalink;?>">
+
+    <title><?=$metatitle;?></title>
+    <link rel="stylesheet" type="text/css" href="assets/css/app.css">
+</head>
+<body>
+    <div class="logo"><img src="assets/images/logo.png" alt="Sourkick logo"></div>
+    <div class="slogan"><div><h1>COMING SOON</h1><span>Are you ready?</span></div></div>
+</body>
+</html>
